119ddfcfab0e4d585d1bf16604fbdeb30d9e5dd2
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-04-01 18:24:34 +0500

    initial commit

---
+++ AdmissionSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdmissionSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
