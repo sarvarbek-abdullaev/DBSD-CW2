5cbd0512f9fbe9fa0bba2543dee09d0002071c8c
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-04-01 18:45:28 +0500

    index page

---
+++ AdmissionSystem/Controllers/TeacherController.cs
@@ -0,0 +1,91 @@
+﻿using AdmissionSystem.DAL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdmissionSystem.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly ITeacherRepository _repository;
+        public TeacherController(ITeacherRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: TeacherController
+        public ActionResult Index()
+        {
+            var teachers = _repository.GetAll();
+            return View(teachers);
+        }
+
+        // GET: TeacherController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TeacherController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TeacherController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TeacherController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TeacherController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TeacherController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TeacherController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
