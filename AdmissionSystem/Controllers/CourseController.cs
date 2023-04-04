d3539f0ebfa8e8bfcaf52062c88eac636023739b
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2023-04-04 18:44:36 +0500

    course added

---
+++ AdmissionSystem/Controllers/CourseController.cs
@@ -0,0 +1,83 @@
+﻿using AdmissionSystem.DAL;
+using AdmissionSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdmissionSystem.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseRepository _repository;
+        public CourseController(ICourseRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ActionResult Index()
+        {
+            var courses = _repository.GetAll();
+            return View(courses);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var course = _repository.GetCourseById(id);
+            return View(course);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Course course)
+        {
+            try
+            {
+                int id = _repository.Insert(course);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- AdmissionSystem/DAL/IClassRepository.cs
+++ AdmissionSystem/DAL/IClassRepository.cs
@@ -9,11 +9,5 @@ namespace AdmissionSystem.DAL
         List<Class> GetAll();
         int Insert(Class @class);
         Class GetClassById(int Id);
-
-        //List<Class> Filter(
-        //    string firstname, string secondname,
-        //    out int totalRows,
-        //    int page = 1, int pageSize = 10,
-        //    string sortColumn = "TeacherId", bool sortDesc = false);
     }
 }
